4f71fc6a93558dec3e868a1fece9a62b084a1aff
mohamadbhintang <102362464+bhntang@users.noreply.github.com>
2023-03-20 08:08:41 +0700

    Add files via upload

---
+++ Penjumalahn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul5_1302200091
+{
+    internal class Penjumalahn
+    {
+        public static T jumlahTigaAngka <T>(T input1, T input2, T input3)
+        {
+            dynamic a = input1;
+            dynamic b = input2;
+            dynamic c = input3;
+
+            return input1 + input2 + input3
+        }
+    }
+
+    internal class SimpleDatabase
+}
